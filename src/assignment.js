662ff1923817b2791f4dcf585d1f40708f0af627
Amalachukwu Precious, William <104999315+PrecyWil@users.noreply.github.com>
2022-07-07 14:04:01 +0000

    check challenge - 1

--- src/assignment.js
+++ src/assignment.js
@@ -13,10 +13,15 @@ const assignment = {};
  * @param {Array} arrayOfNumbers the array of numbers to sum
  * @returns number the sum of the numbers
  */
+
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
-}
-// assignment.sumOfNumbers = sumOfNumbers;
+     let sum =0;
+  for (let i =0; i < arrayOfNumbers.length; i++) {
+      sum +=arrayOfNumbers[i]
+  }
+    return sum;
+  }
+ assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -28,9 +33,10 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  let evenNum = arrayOfNumbers.filter(number => number % 2 == 0);
+    return evenNum;
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+ assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
